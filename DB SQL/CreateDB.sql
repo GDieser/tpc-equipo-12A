0cf4a4b6d79a141b0a4948c6d2785f715a6b64f9
GDieser <germandieser@gmail.com>
2025-06-04 23:34:01 -0300

    Agrega Create DB, con tablas para publicaciones e imagenes

---
+++ DB SQL/CreateDB.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE CURSOS_TPC_G12A
+GO
+
+USE CURSOS_TPC_G12A
+GO
+
+CREATE TABLE Categoria(
+	IdCategoria INT IDENTITY(1,1) PRIMARY KEY,
+	Nombre VARCHAR(85) NOT NULL
+);
+
+CREATE TABLE Imagen(
+	IdImagen INT IDENTITY(1,1) PRIMARY KEY,
+	UrlImagen VARCHAR(255),
+	Nombre VARCHAR(85),
+	IdTipoImagen INT NOT NULL
+);
+
+CREATE TABLE Publicacion(
+	IdPublicacion INT IDENTITY(1,1) PRIMARY KEY,
+	IdCategoria INT NOT NULL,
+	IdImagen INT NOT NULL,
+	Titulo VARCHAR(100) NOT NULL,
+	Descripcion TEXT NOT NULL,
+	Resumen VARCHAR(255) NOT NULL,
+	FechaCreacion DATE,
+	FechaPublicacion DATE,
+	Estado BIT,
+
+	FOREIGN KEY (IdCategoria) REFERENCES Categoria(IdCategoria),
+	FOREIGN KEY (IdImagen) REFERENCES Imagen(IdImagen)
+);
+
+CREATE TABLE ImagenPublicacion(
+	IdImagen INT NOT NULL,
+	IDPublicacion INT NOT NULL,
+	PRIMARY KEY (IdImagen, IDPublicacion),
+	FOREIGN KEY (IdImagen) REFERENCES Imagen(IdImagen),
+	FOREIGN KEY (IDPublicacion) REFERENCES Publicacion(IdPublicacion)
+);
